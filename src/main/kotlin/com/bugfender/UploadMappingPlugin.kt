c59a4ad21ca53963b9f064a332f37d2002c28647
KenjiTakahashi <wozniakk@gmail.com>
2021-09-30 22:23:39 +0200

    improve logging

--- src/main/kotlin/com/bugfender/UploadMappingPlugin.kt
+++ src/main/kotlin/com/bugfender/UploadMappingPlugin.kt
@@ -7,19 +7,24 @@ import org.gradle.api.Project
 class UploadMappingPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val config = project.extensions.create("bugfender", UploadMappingPluginExtension::class.java)
+        project.logger.debug("[Bugfender] Activating plugin")
 
         project.pluginManager.withPlugin("com.android.application") {
             val app = project.extensions.getByType(AppExtension::class.java)
+            project.logger.debug("[Bugfender] Found an Android application")
+
             // This makes sure that we get Variants after they are populated
             project.afterEvaluate {
                 app.applicationVariants.configureEach { variant ->
+                    project.logger.debug("[Bugfender] Applying plugin to {}", variant.name)
+
                     if (!variant.buildType.isMinifyEnabled) {
-                        project.logger.debug("Minification is disabled for {}", variant.name)
+                        project.logger.lifecycle("[Bugfender] Not applying to {}, minification is disabled", variant.name)
                         return@configureEach
                     }
 
                     val assembleTask = variant.assembleProvider.orNull ?: run {
-                        project.logger.warn("No assemble provider available for {}", variant.name)
+                        project.logger.warn("[Bugfender] No assemble provider available for {}", variant.name)
                         return@configureEach
                     }
 
@@ -30,6 +35,8 @@ class UploadMappingPlugin : Plugin<Project> {
                         constructor,
                     )
                     assembleTask.finalizedBy(task)
+
+                    project.logger.debug("[Bugfender] Applied plugin to {}", variant.name)
                 }
             }
         }
